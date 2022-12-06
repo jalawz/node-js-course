932d189b6385ac54942dab155ec2219d4e5740a0
Paulo Roberto Menezes <paulomenezes.web@gmail.com>
2022-12-06 11:55:30 -0300

    Finished chapter 05

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
-const http = require('http');
+const path = require('path');
+
 const express = require('express');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+// Create express app
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next();
-});
+// Add express middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Register routes
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    console.log('In another middleware');
-    res.send('<h1>Hello from Express</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
