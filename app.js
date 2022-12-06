83f30262bb36d9b22002fffc768776e5582e9544
Paulo Roberto Menezes <paulomenezes.web@gmail.com>
2022-12-06 14:16:12 -0300

    Chapter 06 - 6.3 installing and implementing pug

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const shopRoutes = require('./routes/shop');
 // Create express app
 const app = express();
 
+// Register template engines
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 // Add express middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
