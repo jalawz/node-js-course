255af2cb21c9817ccc1f40a9ab65afdf91e2dc2e
Paulo Roberto Menezes <paulomenezes.web@gmail.com>
2022-12-07 17:32:46 -0300

    Chapter 06 - 6.9 finishing pug template

--- routes/shop.js
+++ routes/shop.js
@@ -10,7 +10,7 @@ router.get('/', (req, res, next) => {
     const products = adminData.products;
     res.render('shop', { 
         prods: products,
-        docTitle: 'Shop' 
+        path: '/shop'
     });
 });
 
