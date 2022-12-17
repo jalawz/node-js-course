386c0bfff7bd6a173e41e39e39e9de9ed17f5c2f
Paulo Roberto Menezes <paulomenezes.web@gmail.com>
2022-12-17 20:29:06 -0300

    Chapter 08 - 8.4 registering routes

--- controllers/admin.js
+++ controllers/admin.js
@@ -17,14 +17,11 @@ exports.postAddProduct = (req, res, next) => {
 }
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll((products) => {
-        res.render('shop/product-list', {
+    Product.fetchAll(products => {
+        res.render('admin/products', {
             prods: products,
-            pageTitle: 'Shop',
-            path: '/',
-            hasProducts: products.length > 0,
-            activeShop: true,
-            productCSS: true
+            pageTitle: 'Admin Products',
+            path: '/admin/products'
         });
     });
 }
