a936c8189b37afb342dd9935ab2d2f0add626237
Paulo Roberto Menezes <paulomenezes.web@gmail.com>
2022-12-25 19:53:16 -0300

    Chapter 09 - 9.10 using query params

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('admin/add-product', {
+    res.render('admin/edit-product', {
         pageTitle: 'Add Product',
         path: '/admin/add-product',
         formsCSS: true,
@@ -15,6 +15,18 @@ exports.postAddProduct = (req, res, next) => {
     const product = new Product(title, imageUrl, description, price);
     product.save();
     res.redirect('/');
+}
+
+exports.getEditProduct = (req, res, next) => {
+    const { edit } = req.query
+    if (!edit) {
+        return res.redirect('/');
+    }
+    res.render('admin/edit-product', {
+        pageTitle: 'Edit Product',
+        path: '/edit/add-product',
+        editing: edit
+    });
 }
 
 exports.getProducts = (req, res, next) => {
