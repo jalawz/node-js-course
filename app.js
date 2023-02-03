1c0250b5eab571ce61151e0d6ac4162a61783a18
Paulo Roberto Menezes <paulomenezes.web@gmail.com>
2023-02-03 17:17:01 -0300

    Chapter 10 - 10.8 Retrieving data

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 
 const errorController = require('./controllers/error');
+const db = require('./util/database');
 
 // Create express app
 const app = express();
@@ -13,6 +14,14 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+db.execute('SELECT * FROM products')
+    .then(result => {
+        console.log(result[0]);
+    })
+    .catch(err => {
+        console.log(err);
+    });
 
 // Add express middleware
 app.use(express.json());
