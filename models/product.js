977d10899d3f1e49234aa73e864faabbd86be744
Paulo Roberto Menezes <paulomenezes.web@gmail.com>
2023-02-03 21:39:54 -0300

    11 - Inserting data into the database

--- models/product.js
+++ models/product.js
@@ -14,7 +14,10 @@ module.exports = class Product {
     }
 
     save() {
-        
+        return db.execute(
+            `INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)`,
+            [this.title, this.price, this.imageUrl, this.description]
+        );
     }
 
     static deleteById(id) {
