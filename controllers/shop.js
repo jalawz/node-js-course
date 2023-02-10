1a21bfd9754df78d9ad908c7ab0c8d02b8ab69dc
Paulo Roberto Menezes <paulomenezes.web@gmail.com>
2023-02-09 23:05:27 -0300

    24. Storing Cartitems as Orderitems

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,4 @@
 const Product = require('../models/product');
-const Cart = require('../models/cart');
 
 exports.getIndex = (req, res, next) => {
     Product.findAll()
@@ -114,6 +113,29 @@ exports.getOrders = (req, res, next) => {
         path: '/orders',
         pageTitle: 'Your Orders'
     });
+}
+
+exports.postOrder = (req, res, next) => {
+    const { user } = req;
+    let fetchedProducts;
+    user.getCart()
+        .then(cart => {
+            return cart.getProducts();
+        })
+        .then(products => {
+            fetchedProducts = products;
+            return user.createOrder();
+        })
+        .then(order => {
+            return order.addProducts(fetchedProducts.map(product => {
+                product.orderItem = { quantity: product.cartItem.quantity };
+                return product;
+            }));
+        })
+        .then(result => {
+            res.redirect('/orders');
+        })
+        .catch(err => console.error(err));
 }
 
 exports.getCheckout = (req, res, next) => {
