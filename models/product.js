0b0b4ecc9783b5a08a0d797d916e834c4f2c7770
Paulo Roberto Menezes <paulomenezes.web@gmail.com>
2023-02-18 18:59:25 -0300

    12. Fetching a Single Product

--- models/product.js
+++ models/product.js
@@ -1,3 +1,5 @@
+const { ObjectId } = require('mongodb');
+
 const getDb = require('../util/database').getDb;
 
 class Product {
@@ -25,6 +27,18 @@ class Product {
             .then(products => {
                 console.log(products);
                 return products;
+            })
+            .catch(err => console.error(err));
+    }
+
+    static findById(productId) {
+        const db = getDb();
+        return db.collection('products')
+            .find({_id: new ObjectId(productId)})
+            .next()
+            .then(product => {
+                console.log(product);
+                return product;
             })
             .catch(err => console.error(err));
     }
