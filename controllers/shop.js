5b317120d555ab22e8b345eb1746bfc72bb71e02
Paulo Roberto Menezes <paulomenezes.web@gmail.com>
2023-02-24 23:02:04 -0300

    25. Fixing a Bug

--- controllers/shop.js
+++ controllers/shop.js
@@ -58,6 +58,7 @@ exports.postCart = (req, res, next) => {
         })
         .then(result => {
             console.log(result);
+            res.redirect('/cart');
         })
         .catch(err => console.error(err));
 
