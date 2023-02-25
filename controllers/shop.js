3c1a389881bcea4e493da595b7dfc6bec9710f8a
Paulo Roberto Menezes <paulomenezes.web@gmail.com>
2023-02-24 23:09:33 -0300

    26. Deleting Cart Items

--- controllers/shop.js
+++ controllers/shop.js
@@ -52,62 +52,22 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
     const { productId } = req.body;
+    const { user } = req;
     Product.findById(productId)
         .then(product => {
-            return req.user.addToCart(product);
+            return user.addToCart(product);
         })
         .then(result => {
             console.log(result);
             res.redirect('/cart');
         })
         .catch(err => console.error(err));
-
-    // const { user } = req;
-    // let fetchedCart;
-    // let newQuantity = 1;
-
-    // user.getCart()
-    //     .then(cart => {
-    //         fetchedCart = cart;
-    //         return cart.getProducts({ where: { id: productId }})
-    //     })
-    //     .then(products => {
-    //         let product;
-    //         if (products.length > 0) {
-    //             product = products[0];
-    //         }
-            
-    //         if (product) {
-    //             const oldQuantity = product.cartItem.quantity;
-    //             newQuantity = oldQuantity + 1;
-    //             return product;
-    //         }
-    //         return Product.findByPk(productId);
-    //     })
-    //     .then(product => {
-    //         return fetchedCart.addProduct(product, { through: { quantity: 
-    //             newQuantity
-    //         }   
-    //     });
-    //     })
-    //     .then(() => {
-    //         res.redirect('/cart')
-    //     })
-    //     .catch(err => console.error(err));
-    
 }
 
 exports.postCartDeleteProduct = (req, res, next) => {
     const { productId } = req.body;
     const { user } = req;
-    user.getCart()
-        .then(cart => {
-            return cart.getProducts({ where: { id: productId }})
-        })
-        .then(products => {
-            const product = products[0];
-            return product.cartItem.destroy();
-        })
+    user.deleteItemFromCart(productId)
         .then(result => {
             res.redirect('/cart');
         })
