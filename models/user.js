31d3503f613056b9632c88ae31b953074496940f
Paulo Roberto Menezes <paulomenezes.web@gmail.com>
2023-02-24 23:56:02 -0300

    28. Adding Relational Order Data

--- models/user.js
+++ models/user.js
@@ -81,7 +81,17 @@ class User {
 
     addOrder() {
         const db = getDb();
-        return db.collection('orders').insertOne(this.cart)
+        return this.getCart()
+            .then(products => {
+                const order = {
+                    items: products,
+                    user: {
+                        _id: new ObjectId(this._id),
+                        name: this.name
+                    }
+                }
+                return db.collection('orders').insertOne(order);
+            })
             .then(result => {
                 this.cart = {items: []};
                 return db.collection('users')
@@ -90,6 +100,11 @@ class User {
                         {$set: {cart: { items: [] }} }
                     );
             });
+    }
+
+    getOrders() {
+        const db = getDb();
+        // return db.collection('orders')
     }
 
     static findById(userId) {
