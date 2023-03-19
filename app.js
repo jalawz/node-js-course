df52e75c4cacaf6d790766274b824121b042cee1
Paulo Roberto Menezes <paulomenezes.web@gmail.com>
2023-03-19 18:09:43 -0300

    10. Adding and Using a User Model

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
 
-// const User = require('./models/user');
+const User = require('./models/user');
 
 // Create express app
 const app = express();
@@ -23,14 +23,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//     User.findById('63f7d16bed3d4220ca3ef653')
-//         .then(user => {
-//             req.user = new User(user.name, user.email, user.cart, user._id);
-//             next();
-//         })
-//         .catch(err => console.error(err));
-// })
+app.use((req, res, next) => {
+    User.findById('641778fd57d7758cc61e7221')
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.error(err));
+});
 
 // Register routes
 app.use('/admin', adminRoutes);
@@ -43,6 +43,18 @@ mongoose.connect('mongodb://paulo:penha269@127.0.0.1:27017', {
     })
     .then(result => {
         console.log('Connected!')
-        app.listen(3000)
+        User.findOne().then(user => {
+            if (!user) {
+                const user = new User({
+                    name: 'Max',
+                    email: 'max@test.com',
+                    cart: {
+                        items: []
+                    }
+                });
+                user.save();
+            }
+        });
+        app.listen(3000);
     })
     .catch(err => console.error(err));
