d3cece6bb598537944060f7c6c6a0f2e77bb7570
Paulo Roberto Menezes <paulomenezes.web@gmail.com>
2023-03-20 16:02:54 -0300

    5. Adding the Request Driven Login Solution

--- controllers/shop.js
+++ controllers/shop.js
@@ -7,7 +7,8 @@ exports.getIndex = (req, res, next) => {
             res.render('shop/index', {
                 prods: products,
                 pageTitle: 'Shop',
-                path: '/'
+                path: '/',
+                isAuthenticated: req.isLoggedIn
             });
         })
         .catch(err => console.error(err));
@@ -19,7 +20,8 @@ exports.getProducts = (req, res, next) => {
             res.render('shop/product-list', {
                 prods: products,
                 pageTitle: 'All Products',
-                path: '/products'
+                path: '/products',
+                isAuthenticated: req.isLoggedIn
             });
         })
         .catch(err => console.error(err));
@@ -32,7 +34,8 @@ exports.getProduct = (req, res, next) => {
             res.render('shop/product-detail', {
                 product: product,
                 pageTitle: product.title,
-                path: "/products"
+                path: "/products",
+                isAuthenticated: req.isLoggedIn
             });
         })
         .catch(err => console.log(err));
@@ -46,7 +49,8 @@ exports.getCart = (req, res, next) => {
             res.render('shop/cart', {
                 path: '/cart',
                 pageTitle: 'Your Cart',
-                products: products
+                products: products,
+                isAuthenticated: req.isLoggedIn
             });    
         })
         .catch(err => console.error(err));
@@ -111,7 +115,8 @@ exports.getOrders = (req, res, next) => {
             res.render('shop/orders', {
                 path: '/orders',
                 pageTitle: 'Your Orders',
-                orders: orders
+                orders: orders,
+                isAuthenticated: req.isLoggedIn
             });
         })
         .catch(err => console.error(err));
