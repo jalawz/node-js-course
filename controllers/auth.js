4db019cef8c2d8f361885aea4f05bc58a12c61ea
Paulo Roberto Menezes <paulomenezes.web@gmail.com>
2023-03-22 11:56:29 -0300

    11. Using the Session Middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,12 @@
 exports.getLogin = (req, res, next) => {
-    const isLoggedIn = req.get('Cookie').trim().split('=')[1] === 'true';
-    console.log(isLoggedIn);
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
-        isAuthenticated: isLoggedIn
+        isAuthenticated: false
     });
 }
 
 exports.postLogin = (req, res, next) => {
-    res.setHeader('Set-Cookie', 'loggedIn=true');
+    req.session.isLoggedIn = true;
     res.redirect('/');
 }
