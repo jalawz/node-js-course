429591c186a4d9227c8fa2c223fde87c6958a3e5
Paulo Roberto Menezes <paulomenezes.web@gmail.com>
2023-06-14 17:06:15 -0300

    8. Adding the Signin Functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -19,13 +19,30 @@ exports.getSignup = (req, res, next) => {
 }
 
 exports.postLogin = (req, res, next) => {
-    User.findById('641778fd57d7758cc61e7221')
+    const { email, password } = req.body
+
+    User.findOne({ email: email })
         .then(user => {
-            req.session.isLoggedIn = true;
-            req.session.user = user;
-            req.session.save(err => {
-                res.redirect('/');
-            });
+            if (!user) {
+                return res.redirect('/login');
+            }
+
+            bcrypt.compare(password, user.password)
+                .then(doMatch => {
+                    if (doMatch) {
+                        req.session.isLoggedIn = true;
+                        req.session.user = user;
+                        return req.session.save(err => {
+                            console.log(err);
+                            return res.redirect('/');
+                        });
+                    }
+                    res.redirect('/login');
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.redirect('/login');
+                });
         })
         .catch(err => console.log(err));
 }
